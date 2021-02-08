599c807586ad420f95cf016cd6f32123803b75ec
abhirajthakur <abhirajthakur124@gmail.com>
2021-02-08 12:10:18 +0530

    small changes in entry screen

--- calculator.py
+++ calculator.py
@@ -58,7 +58,7 @@ frame.grid()
 frame2 = Frame(frame, bd=15, pady=5, relief=RIDGE, bg='red')
 frame2.grid()
 
-MainFrame = Frame(frame2, bd=6, pady=3, relief=RIDGE, bg='#00BFFF')
+MainFrame = Frame(frame2, bd=7, pady=3, relief=RIDGE, bg='#00BFFF')
 MainFrame.grid()
 
 
@@ -67,59 +67,59 @@ MainFrame.grid()
 screen_value = StringVar()
 screen_value.set("")
 screen = Entry(MainFrame, width=14,textvar=screen_value, font=('arial', 35, BOLD), fg='white', bd=12, bg='#00BFFF', justify=RIGHT)
-screen.grid(row=0, column=0, columnspan=5, ipady=8)
+screen.grid(row=0, column=0, columnspan=5, ipady=7)
 
 numberpad = '789456123'
 i = 0
 btn = []
 for j in range(3, 6):
     for k in range(3):
-        btn.append(Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text=numberpad[i]))
+        btn.append(Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text=numberpad[i]))
         btn[i].grid(row=j, column=k)
         btn[i].bind("<Button-1>", click)
         i+=1
 
-clear_all = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='C')
+clear_all = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='C')
 clear_all.grid(row=2, column=0)
 clear_all.bind("<Button-1>", click)
 
-backspace = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='⌫')
+backspace = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='⌫')
 backspace.grid(row=2, column=1)
 backspace.bind("<Button-1>", click)
 
-plus_minus = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='±')
+plus_minus = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='±')
 plus_minus.grid(row=2, column=2)
 plus_minus.bind("<Button-1>", click)
 
-divide = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='÷')
+divide = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='÷')
 divide.grid(row=2, column=3)
 divide.bind("<Button-1>", click)
 
-multiply = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='x')
+multiply = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='x')
 multiply.grid(row=3, column=3)
 multiply.bind("<Button-1>", click)
 
-minus = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='-')
+minus = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='-')
 minus.grid(row=4, column=3)
 minus.bind("<Button-1>", click)
 
-plus = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='+')
+plus = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='+')
 plus.grid(row=5, column=3)
 plus.bind("<Button-1>", click)
 
-square_root = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='√')
+square_root = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='√')
 square_root.grid(row=6, column=0)
 square_root.bind("<Button-1>", click)
 
-zero = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='0')
+zero = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='0')
 zero.grid(row=6, column=1)
 zero.bind("<Button-1>", click)
 
-point = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='.')
+point = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='.')
 point.grid(row=6, column=2)
 point.bind("<Button-1>", click)
 
-equals = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=16, bd=6, text='=')
+equals = Button(MainFrame, fg='white', bg='#00BFFF',width=3, height=2, font=('arial', 19, BOLD), padx=17, bd=7, text='=')
 equals.grid(row=6, column=3)
 equals.bind("<Button-1>", click)
 
